a42fb2995bbf5f59704ba69adb451e8447317a2f
prakhar test <mr.prakhar@gmail.com>
2026-02-11 08:30:43 +0530

    After Restructuring the website
    After Restructuring the website

--- MyPortfolio/myapp/views.py
+++ MyPortfolio/myapp/views.py
@@ -1,3 +1,4 @@
+import traceback
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.http import HttpResponse,HttpRequest,JsonResponse
@@ -12,13 +13,14 @@ from django.conf import settings
 from pathlib import Path
 import json
 import MySQLdb
+import asyncio
 import datetime as dt
 from myapp.utils import prepare_email_template_and_send as send_mail
 import os
 from myapp.utils import format_headers
 #To fetch and store the IP and location of the user
 import pandas as pd
-
+import threading
 from django.http import JsonResponse
 import psycopg2  # Example for PostgreSQL, adapt according to your database
 
@@ -31,45 +33,69 @@ import psycopg2  # Example for PostgreSQL, adapt according to your database
 
 base_dir=Path(__file__).resolve().parent
 # print("current dir : ",base_dir)
-IMAGE_FOLDER = os.path.join(base_dir, 'static/images')
-TEXT_FOLDER = os.path.join(base_dir, 'static/text')
-PDF_FOLDER = os.path.join(base_dir, 'static/pdf')
+IMAGE_FOLDER = os.path.join(base_dir, '../staticfiles/images')
+TEXT_FOLDER = os.path.join(base_dir, '../staticfiles/text')
+PDF_FOLDER = os.path.join(base_dir, '../staticfiles/pdf')
 
 print("Static url : ",settings.STATIC_URL,IMAGE_FOLDER)
 # Create your views here.
 def home(request):
     return render(request , "home.html")
 
+def fetch_link(des:tuple):
+    list_of_links = []
+   
+    for name, url in des:
+        
+        links_dict = {}
+        links_dict["name"] = name.strip()
+        links_dict["url"] = url.strip()
+        list_of_links.append(links_dict)
+    return list_of_links
+
 def get_images(request):
+    try:
 
-    images = []
-    
-    if os.path.exists(IMAGE_FOLDER): 
+        images = []
+        
+        if os.path.exists(IMAGE_FOLDER): 
 
-        # print("Exists",os.listdir(IMAGE_FOLDER)) # Ensure folder exists
-        for filename in os.listdir(IMAGE_FOLDER):
-            # print("CHECKING : ",filename)
-            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp')) and "slideshow1" in filename.lower():
-                desc_file = os.path.splitext(filename)[0] + '.txt'
-                desc_path = os.path.join(TEXT_FOLDER, desc_file)
-                description = open(desc_path).read().strip() if os.path.exists(desc_path) else "No description available"
-                # description =description.split("*****")
-                # description = "\n".join(description)
-                # print("ADDING : ",f'images/{filename}')
-                # with open("paths.txt","a") as file:
-                #     file.write( f'\nimages/{filename}')
-                appname = filename.split("_")[0]
-                appname = filename.split("slideshow1")[0]
+            # print("Exists",os.listdir(IMAGE_FOLDER)) # Ensure folder exists
+            for filename in os.listdir(IMAGE_FOLDER):
+                # print("CHECKING : ",filename)
+            
+                if filename.lower().endswith(('.png', '.jpg', '.jpeg')) and "slideshow1" in filename.lower():
+                    desc_file = os.path.splitext(filename)[0] + '.txt'
+                    desc_path = os.path.join(TEXT_FOLDER, desc_file)
+                    description = open(desc_path).read().strip() if os.path.exists(desc_path) else "No description available"
+                    
+                    linkname_regex = '>>@link\|([^|]+)\|\s*@startlink\|([^|]+)\|@endlink\|'
+                    links_list  = re.findall(linkname_regex,description,re.IGNORECASE)
+                    description = re.sub(linkname_regex,'',description)
+                    if links_list:
+                        print("LINKS FOUND : ",links_list)
+                        links_dict_list= fetch_link(links_list)
+                    else:
+                        links_dict_list = []
+                    appname = filename.split("_")[0]
 
-                images.append({'src': f'images/{filename}','desc': description, 'filename':appname})
-                
-    else:
-        print("Image Folder Not Found!",IMAGE_FOLDER)
-    response_object = JsonResponse(images, safe=False)
-        #     print("\nResponse : ",
-        # json.loads(response_object.content)
-        # )
-    return JsonResponse(images, safe=False)
+                    appname = filename.split("slideshow1")[0]
+                    print("LINKS : ",links_dict_list)
+                    images.append({'src': f'images/{filename}','desc': description, 'filename':appname,'links':links_dict_list})
+                    
+        else:
+            print("Image Folder Not Found!",IMAGE_FOLDER)
+        response_object = JsonResponse(images, safe=False)
+            #     print("\nResponse : ",
+            # json.loads(response_object.content)
+            # )
+        return JsonResponse(images, safe=False)
+    except Exception as e:
+        print("Exception occured in get images!!",e)
+        traceback.print_exc()
+        response_object = JsonResponse({'src': 'images/logo.png','desc': "Uh Oh! The description for this slide is unavilable at the moment :(",
+                                        "filename":"Failed to fetch slide","links":[]}, safe=False)
+        return response_object
 
 
 def get_events(request):
@@ -148,7 +174,7 @@ def get_certificates(request):
                 desc_file = os.path.splitext(filename)[0] + '.txt'
                 desc_path = os.path.join(TEXT_FOLDER, desc_file)
                 description = open(desc_path).read().strip() if os.path.exists(desc_path) else "No description available"
-                # print("ADDING : ",f'\npdf/{filename}')
+                print("ADDING : ",f'\npdf/{filename}')
                 # with open("paths.txt","a") as file:
                 #     file.write( f'\npdf/{filename}')
 
